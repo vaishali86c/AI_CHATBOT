03b5919a99fdc8489082b015d4303c7d03a25ac6
vaishali86c <vaishali86c@gmail.com>
2024-12-31 22:33:18 +0530

    Verify Token Functionality

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -1,9 +1,36 @@
+import { Request, Response, NextFunction } from "express-serve-static-core";
 import jwt from "jsonwebtoken";
+import { COOKIE_NAME } from "./constants.js";
 
 export const createToken = (id: string, email: string, expiresIn: string) => {
     const payload = { id, email };
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn,
     });
+    console.log(token)
     return token;
 };
+
+export const verifyToken = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {    
+    const token = req.signedCookies[`${COOKIE_NAME}`];
+    console.log("token: ", req.signedCookies);
+    if (!token || token.trim() === "") {
+      return res.status(401).json({ message: "Token Not Received" });
+    }
+    return new Promise<void>((resolve, reject) => {
+      return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+        if (err) {
+          reject(err.message);
+          return res.status(401).json({ message: "Token Expired" });
+        } else {
+          resolve();
+          res.locals.jwtData = success;
+          return next();
+        }
+      });
+    });
+  };
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useContext, useEffect, useState } from 'react'
-import { loginUser } from '../helpers/api-communicator';
+import { checkAuthStatus, loginUser } from '../helpers/api-communicator';
 
 type User = {
     name: string;
@@ -20,6 +20,14 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
     useEffect(() => {
         //fetch if the user's cokkies are valid then skip login
+        async function checkStatus() {
+            const data = await checkAuthStatus();
+            if (data) {
+                setUser({ email:data.email, name: data.name})
+                setIsLoggedIn(true);
+            }
+        }
+        checkStatus();
     }, []);
 
     const login = async(email: string, password: string) => {
